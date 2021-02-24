eda977464ec8a30ccc4b5fef25a505c17105a6ab
Ryan <ryanperryj@gmail.com>
2021-02-24 10:04:17 -0500

    Character controller improvements

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,58 +4,31 @@ var sz_tl = Globals.sz_tl
 
 var god_mode = false
 
-var ACCELERATION = sz_tl*250
-var GRAVITY = sz_tl*60
-var FRICTION = sz_tl*750
-var MOVE_SPEED = sz_tl*12.5
-var FALL_SPEED = sz_tl*25
+const ACCELERATION = 1000
+const GRAVITY = 1000
+const FRICTION = 1000
+const MAX_SPEED = 150
+const FALL_SPEED = 500
+const JUMP_FORCE = 400
+
 var input_vector = Vector2.ZERO
 var velocity = Vector2.ZERO
 
-var JUMP_FORCE = sz_tl*30
-
 func _physics_process(delta):
-#
-#	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-#	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-#	input_vector = input_vector.normalized()
-#
-#	if god_mode:
-#		$CollisionShape2D.disabled = true
-#
-#		if input_vector != Vector2.ZERO:
-#			velocity = velocity.move_toward(MAX_SPEED * input_vector, ACCELERATION * delta)
-#		else:
-#			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-#	else:
-#		$CollisionShape2D.disabled = false
-#
-#		if Input.is_action_just_pressed("jump"):
-#			velocity.y = -5*MAX_SPEED
-#
-#		if input_vector != Vector2.ZERO:
-#			velocity.x = move_toward(velocity.x, MAX_SPEED * input_vector.x, ACCELERATION * delta)
-#		else:
-#			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-#
-#		velocity.y = move_toward(velocity.y, FALL_SPEED, GRAVITY * delta)
+	var x_input = Input.get_action_strength("right") - Input.get_action_strength("left")
 	
-	$CollisionShape2D.disabled = god_mode
-	
-	if Input.is_action_pressed("right") and !Input.is_action_pressed("left"):
-		velocity.x = MOVE_SPEED 
-		$Sprite.flip_h = true
-		$AnimationPlayer.play("run")
-	elif Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
-		velocity.x = -MOVE_SPEED
-		$Sprite.flip_h = false
-		$AnimationPlayer.play("run")
-	else:
-		velocity.x = 0
+	if x_input == 0:
 		$AnimationPlayer.play("idle")
+		velocity.x = move_toward(velocity.x, 0.0, FRICTION * delta)
+	else:
+		$AnimationPlayer.play("run")
+		$Sprite.flip_h = x_input > 0
+		velocity.x = move_toward(velocity.x, MAX_SPEED * x_input, ACCELERATION * delta)
 	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -JUMP_FORCE
+	#if on_floor:
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -JUMP_FORCE
 	
 	velocity.y = move_toward(velocity.y, FALL_SPEED, GRAVITY * delta)
 	
